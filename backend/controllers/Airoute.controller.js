3e427f1c04061a7c5bfced4a03924f8ed51a6b44
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-03 16:53:28 -0500

    reorganized

---
+++ backend/controllers/Airoute.controller.js
@@ -0,0 +1,29 @@
+const OpenAI = require("openai");
+const client = new OpenAI({
+  apiKey: process.env.OPENAIKEY,
+});
+
+const AIfunction = async (req, res) => { 
+    try {  
+const { userRes } = req.body;
+
+    if (!userRes || userRes === ""){ 
+        return res.status(404).json({ 
+            error: 'No valid input'
+        }); 
+    }  
+    const response = await client.chat.completions.create({ 
+        model: 'gpt-5-nano', 
+        messages: [{ role: 'user', content: userRes}]
+    });  
+    res.send({ response: response.choices[0].message.content });
+
+    } catch (error) { 
+        console.error('Error with sending response to AI'); 
+        res.status(501).json({ 
+            Error: error
+        }); 
+    }; 
+} 
+
+module.exports = AIfunction; 
--- backend/routes/AIroute.js
+++ backend/routes/AIroute.js
@@ -1,31 +1,6 @@
 const express = require("express");
 const airoute = express.Router();
-const OpenAI = require("openai");
-const client = new OpenAI({
-  apiKey: process.env.OPENAIKEY,
-});
+const AicontrollerFunction = require('../controllers/Airoute.controller.js'); 
 
-airoute.post("/AI", async (req, res) => {
-  // set up the route here to get data from the body as such
-  try {
-    const { userRes } = req.body;
-
-    if (!userRes || userRes === ""){ 
-        return res.status(404).json({ 
-            error: 'No valid input'
-        }); 
-    } 
-    const response = await client.chat.completions.create({ 
-        model: 'gpt-5-nano', 
-        messages: [{ role: 'user', content: userRes}]
-    });  
-    res.send({ response: response.choices[0].message.content }); 
-  } catch (error) {
-    console.error("Error with getting AI response", error); 
-    res.status(500).json({ 
-        error: 'failed to get AI response'
-    }); 
-  }; 
-});
-
+airoute.post("/AI", AicontrollerFunction); 
 module.exports = airoute;
