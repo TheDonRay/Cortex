5c44e0b9c7717c12481facce8828fc12e1112ee8
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-03 15:49:18 -0500

    working on setting up cortex

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config(); 
+const app = require('./app.js'); 
+
+const PORT = process.env.PORT || 4500; 
+app.listen(PORT, () => { 
+    console.log(`Server successfully running on ${PORT}`); 
+}); 
+
