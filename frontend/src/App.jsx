5aaa3340123f201dff1a4060ac55eb23ba94fd23
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-03 19:22:26 -0500

    working on this

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-
-import './App.css';
+import './App.css'; 
+import Homepage from "./Components/Homepage.jsx"; 
 
 function App() {
   return (
-    <>
+    <> 
+      <Homepage/> 
     </>
   )
 }
---
+++ frontend/src/Components/Homepage.jsx
@@ -0,0 +1,12 @@
+export default function Homepage() { 
+    return( 
+        <>  
+        <div> 
+            <h1> 
+                Cortex 
+            </h1>
+        </div>
+        </>
+    );
+}; 
+
